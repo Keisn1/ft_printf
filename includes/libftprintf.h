51c21a5ea2ea930f79abd89caab10898127988f0
Kay Freyer <kay.freyer@icloud.com>
2024-09-03 15:44:55 +0200

    rename header file

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 int	ft_printf(const char *fmt_string, ...);
 
