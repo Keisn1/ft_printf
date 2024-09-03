dd7a01763e5879a69388e57510cc5cf7f5e6acb7
Kay Freyer <kay.freyer@icloud.com>
2024-09-03 16:13:55 +0200

    conversion i

--- tests/test_ft_printf.cpp
+++ tests/test_ft_printf.cpp
@@ -26,6 +26,8 @@ TEST(ft_printf_test, integer_conversion) {
     compare_printf("Hello %d", -976);
     compare_printf("Hello %d", 0);
     compare_printf("Hello %d", -8);
+
+    compare_printf("Hello %i", -8);
 }
 
 TEST(ft_printf_test, pointer_conversion) {
@@ -33,6 +35,12 @@ TEST(ft_printf_test, pointer_conversion) {
     compare_printf("Hello %p", &d);
     compare_printf("Hello %p", 0);
     compare_printf("Hello %p", 1);
+    compare_printf("Hello %p", -1);
+}
+
+TEST(ft_printf_test, conversion_combos) {
+    int d = 1234;
+    compare_printf("eins %s zwei %c drei %d vier %p", "world", 'q', d, &d);
 }
 
 void compare_printf_wo_args(const char* fmt_string) {
