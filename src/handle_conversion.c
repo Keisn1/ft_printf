45ec21e8f6122416e9ed5c663f7c94575ca23c99
Kay Freyer <kay.freyer@icloud.com>
2024-09-06 21:17:40 +0200

    refactoring handle_conversion

--- src/handle_conversion.c
+++ src/handle_conversion.c
@@ -13,6 +13,34 @@
 #include "ft_printf.h"
 #include "libft.h"
 
+const char* check_zero_padding(bool *zero_padding, const char* p) {
+	if (*p == '0')
+	{
+		*zero_padding = true;
+		p++;
+	}
+	return p;
+}
+
+const char *check_padded_right(bool *padded_left, bool *zero_padding, const char *p) {
+	if (*p == '-')
+	{
+		*padded_left = false;
+		*zero_padding = false;
+		p++;
+	}
+	return p;
+}
+
+void check_field_width(int *min_width, bool *padded_left, bool *zero_padding) {
+	if (*min_width < 0)
+	{
+		*padded_left = false;
+		*zero_padding = false;
+		*min_width = -(*min_width);
+	}
+}
+
 char	*handle_conversion_specifier(va_list ap, char specifier, int prec)
 {
 	if (specifier == '%')
@@ -34,38 +62,48 @@ char	*handle_conversion_specifier(va_list ap, char specifier, int prec)
 	return (ft_strdup(""));
 }
 
+int print_str(va_list ap, const char* p, int prec, bool was_char, bool padded_left, int min_width, bool zero_padding) {
+	char* str;
+	int width;
+
+	str = handle_conversion_specifier(ap, *p, prec);
+
+	if (was_char)
+		width = 1;
+	else
+		width = ft_strlen(str);
+
+	if (padded_left)
+		width = pad(width, min_width, zero_padding);
+
+	if (was_char)
+		ft_putchar_fd(*str, STDOUT_FILENO);
+	else
+		ft_putstr_fd(str, STDOUT_FILENO);
+
+	if (!padded_left)
+		width = pad(width, min_width, zero_padding);
+
+	free(str);
+	return width;
+}
+
 const char	*handle_conversion(va_list ap, const char *p, int *count)
 {
 	int		min_width;
 	int		prec;
 	bool	padded_left;
 	bool	zero_padding;
-	int		width;
 	bool	was_char;
-	char	*str;
 
 	prec = 1;
 	min_width = 0;
 	padded_left = true;
 	zero_padding = false;
-	if (*p == '0')
-	{
-		zero_padding = true;
-		p++;
-	}
-	if (*p == '-')
-	{
-		padded_left = false;
-		zero_padding = false;
-		p++;
-	}
+	p = check_zero_padding(&zero_padding, p);
+	p = check_padded_right(&padded_left, &zero_padding, p);
 	p = extract_int_arg(ap, p, &min_width);
-	if (min_width < 0)
-	{
-		padded_left = false;
-		zero_padding = false;
-		min_width = -min_width;
-	}
+	check_field_width(&min_width, &padded_left, &zero_padding);
 	if (*p == '.')
 		p = extract_int_arg(ap, ++p, &prec);
 	if (is_integer_conversion(*p) && prec != 1)
@@ -73,21 +111,9 @@ const char	*handle_conversion(va_list ap, const char *p, int *count)
 	was_char = false;
 	if (*p == 'c')
 		was_char = true;
-	str = handle_conversion_specifier(ap, *p, prec);
-	if (was_char)
-		width = 1;
-	else
-		width = ft_strlen(str);
-	if (padded_left)
-		width = pad(width, min_width, zero_padding);
-	if (was_char)
-		ft_putchar_fd(*str, STDOUT_FILENO);
-	else
-		ft_putstr_fd(str, STDOUT_FILENO);
-	if (!padded_left)
-		width = pad(width, min_width, zero_padding);
-	free(str);
-	*count += width;
+
+	*count += print_str(ap,  p, prec, was_char, padded_left, min_width, zero_padding) ;
+
 	p++;
 	return (p);
 }
